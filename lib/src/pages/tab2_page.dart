76aeb98f42d3478189ad00dae7e1720b794d2671
Carlos Barcos <fbarcos8@hotmail.com>
2021-03-20 00:36:07 -0500

    Ajustes sobre el diseño de las categorias. 	modificado:     lib/src/pages/tab2_page.dart

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/services/news_service.dart';
 import 'package:provider/provider.dart';
 
@@ -24,16 +25,42 @@ class _ListaCategorias extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (BuildContext context, int index) {
+          final cName = categories[index].name;
           return Padding(
             padding: EdgeInsets.all(8),
             child: Column(
               children: [
-                Icon(categories[index].icon),
+                _CategoryButton(categories[index]),
                 SizedBox(height: 5),
-                Text(categories[index].name)
+                Text('${cName[0].toUpperCase()}${cName.substring(1)}')
               ],
             ),
           );
         });
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+  final Category category;
+
+  const _CategoryButton(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("${category.name}");
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+        child: Icon(
+          category.icon,
+          color: Colors.black54,
+        ),
+      ),
+    );
   }
 }
